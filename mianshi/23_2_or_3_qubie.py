8e8d5ff00ccf83a0e04775611228b1495aefcc42
cmz123 <2394987221@qq.com>
2018-07-28 09:09:13 +0800

    python2和python3的区别

---
+++ mianshi/23_2_or_3_qubie.py
@@ -0,0 +1,17 @@
+"""
+python2和python3的区别：
+链接：http://chenqx.github.io/2014/11/10/Key-differences-between-Python-2-7-x-and-Python-3-x/
+1、__future__模块
+2、print函数
+3、整除
+4、Unicode
+5、xrange模块
+6、python3中的range对象的__contains__方法
+7、raising exceptions
+8、handling exceptions
+9、next（）函数和.next()方法
+10、for循环变量和全局命名空间泄露
+11、比较不可排序类型
+12、通过input（）解析用户的输入
+13、返回可迭代对象，而不是列表
+"""
